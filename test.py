1d5f3655f50b79e7d7adcfbf6e9a49c43b2988bd
wibox <francesco.info.pagano@gmail.com>
2023-02-21 17:08:39 +0100

    INCREDIBILEEEEEEE

--- test.py
+++ test.py
@@ -1,12 +1,7 @@
-import scipy.io.wavfile as siw
 import os
-from glob import glob
-from tqdm import tqdm
-
-from typing import *
-import traceback
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
+import scipy.io.wavfile as siw
 
 LABELS = ["low", "medium", "high"]
 
@@ -29,6 +24,9 @@ def get_data(filename):
 
     audio = tf.squeeze(audio)
 
+    # zero_padding = tf.zeros(sampling_rate - tf.shape(audio), dtype=tf.float32)
+    # audio_padded = tf.concat([audio, zero_padding], axis=0)
+
     return sampling_rate, audio, label
 
 def get_spectrogram(filename):
@@ -50,7 +48,7 @@ def get_spectrogram(filename):
     )
     spectrogram = tf.abs(stft)
 
-    return spectrogram, sampling_rate_float32, label
+    return stft, spectrogram, sampling_rate_float32, label
 
 def get_log_mel_spectrogram(filename):
     frame_length_in_s = 0.04
@@ -58,8 +56,9 @@ def get_log_mel_spectrogram(filename):
     lower_frequency = 20
     upper_frequency = 4000
 
-    spectrogram, sampling_rate_float32, label = get_spectrogram(filename)
+    stft, spectrogram, sampling_rate_float32, label = get_spectrogram(filename)
 
+    # sampling_rate_float32 = tf.cast(sampling_rate, tf.float32)
     frame_length = int(frame_length_in_s * sampling_rate_float32)
     num_spectrogram_bins = frame_length // 2 + 1
 
@@ -86,42 +85,36 @@ def get_log_mel_spectrogram(filename):
 
     return log_mel_spectrogram, label
 
-class DatasetFormatter():
-    def __init__(
-        self,
-        crop_time : int
-    ):
+train_file_ds = tf.data.Dataset.list_files(["complete_training_data/*.wav"])
+train_mel_ds = train_file_ds.map(get_log_mel_spectrogram)
+for spectrogram, label in train_mel_ds.take(1):
+    SHAPE = spectrogram.shape
 
-        self.crop_time = crop_time
-        self.audio_files : List[str] = list()
+def preprocess_with_resized_mel(filename):
+    log_mel_spectrogram, label = get_log_mel_spectrogram(filename)
+    log_mel_spectrogram.set_shape(SHAPE)
+    mfccs = tf.signal.mfccs_from_log_mel_spectrograms(log_mel_spectrogram)[..., :]
+    mfccs = tf.expand_dims(mfccs, -1)
+    mfccs = tf.image.resize(mfccs, [32, 32])
+    label_id = tf.argmax(label == LABELS)
 
-    def _initialize(self):
-        self.audio_files = glob("data/*/*/*.wav")
+    return mfccs, label_id
 
-    def _extract_time_info(self, info_file_path : str):
-        time_info = None
-        try:
-            with open(info_file_path, "r") as info_if:
-                info_data = info_if.readlines()
-                time_info = float(info_data[0].split(" ")[-1])
-        except Exception as e:
-            print(traceback.format_exc())
-        finally:
-            return time_info
+# train_file_ds = tf.data.Dataset.list_files(["complete_training_data/*.wav"])
+train_ds = train_file_ds.map(preprocess_with_resized_mel)
 
-    def _crop_audio(self, audio_path : str):
-        car_label = audio_path.split('/')[2]
-        filename = audio_path.split('/')[-1]
-        if not os.path.exists(f"formatted_data/formatted_{audio_path.split('/')[2]}"):
-            os.makedirs(f"formatted_data/formatted_{car_label}")
-        input_audiofile = siw.read(audio_path)
-        sampling_rate, audio = input_audiofile[0], input_audiofile[1]
-        file_time_info = int(self._extract_time_info(info_file_path = f"{audio_path.split('.')[0]}.txt"))
-        formatted_audio = audio[file_time_info*sampling_rate-sampling_rate*self.crop_time:file_time_info*sampling_rate+sampling_rate*self.crop_time, :]
-        siw.write(filename=f"formatted_data/formatted_{car_label}/{filename}", rate=sampling_rate, data=formatted_audio)
+# TESTING FOR GET_DATA -> ALL OK
+# for sampling_rate, audio, label in train_ds:
+#     print(sampling_rate, audio.dtype, audio.shape)
 
-    def format_dataset(self):
-        self._initialize()
-        for audio_path in tqdm(self.audio_files):
-            self._crop_audio(audio_path = audio_path)
-    
+# TESTING GET_SPECTROGRAM -> TUTTO FORSE OK
+# for stft, spectrogram, sampling_rate, label in train_ds:
+#     print(spectrogram.dtype, spectrogram.shape, stft.dtype, stft.shape)
+
+# TESTING GET_LOG_MEL_SPECTROGRAM -> TUTTO FORSE OK
+# for log_mel_spectrogram, label in train_ds:
+#     print(log_mel_spectrogram.dtype, label)
+
+# TESTING PREPROCESS_WITH_RESIZED_MEL -> 
+for mfccs, label_id in train_ds:
+    print(mfccs.dtype, mfccs.shape)
